c6a43d5ba83ff4ef6090de3938fa77342b171337
SunnyPiase <petya5671@gmail.com>
2022-05-27 20:32:40 +0300

    Clear Main

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -6,20 +6,7 @@ namespace Homework3
     {
         static void Main(string[] args)
         {
-            Vector vector = new Vector(11);
-            vector.RandomInitialization(1, 10);
-            Console.WriteLine(vector);
-            foreach (var item in vector.CalculateFreq())
-            {
-                Console.WriteLine(item);
-            }
 
-            Console.WriteLine(vector);
-            Console.WriteLine(vector.IsPalindrome());
-            vector.Reverse();
-
-            Console.WriteLine(vector);
-            Console.WriteLine($"frequest num :{vector.LongestSubsequence()} times ");
 
         }
     }
