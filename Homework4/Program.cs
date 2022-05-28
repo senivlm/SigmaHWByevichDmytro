c6d0b1f8d3dcd27e2bcac3aa36d78a37b7bfc816
SunnyPiase <petya5671@gmail.com>
2022-05-29 02:00:39 +0300

    Created quick sort with 4 pivots (first, last, middle,"avarage" <- my idea of findig pivot near to median) and implemented test of productivity

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Homework4;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Homework3
 {
@@ -7,12 +10,43 @@ namespace Homework3
     {
         static void Main(string[] args)
         {
-            Vector vector = new Vector(11);
-            vector.RandomInitialization(1, 10);
+            Vector vectorAvaragePivot = new Vector(100000);
+            Vector vectorMiddlePivot = new Vector(100000);
+            var resultTimeAvaragePivot = new List<int>();
+            var resultTimeMiddlePivot = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                vectorAvaragePivot.RandomInitialization(1, 5000, i);
+                vectorMiddlePivot.RandomInitialization(1, 5000, i);
 
-            Console.WriteLine(vector); 
-            vector.Bubble(Trend.increase);
-            Console.WriteLine(vector);
+                var st = Stopwatch.StartNew();
+                vectorAvaragePivot.QuickSort(Pivot.avarage);
+                st.Stop();
+                resultTimeAvaragePivot.Add((int)st.ElapsedMilliseconds);
+
+                Console.WriteLine("pivot is avarage");
+                Console.WriteLine("Is sorted: " + vectorAvaragePivot.IsSorted());
+                Console.WriteLine("time: " + st.ElapsedMilliseconds);
+                Console.WriteLine();
+
+                var st1 = Stopwatch.StartNew();
+                vectorMiddlePivot.QuickSort(Pivot.middleItem);
+                st1.Stop();
+                resultTimeMiddlePivot.Add((int)st1.ElapsedMilliseconds);
+
+                Console.WriteLine("pivot is middle");
+                Console.WriteLine("Is sorted: " + vectorMiddlePivot.IsSorted());
+                Console.WriteLine("time: " + st1.ElapsedMilliseconds);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nAvaragePivotTime - MiddlePivotTime");
+            for (int i = 0; i < resultTimeAvaragePivot.Count; i++)
+            {
+                Console.WriteLine($"{resultTimeAvaragePivot[i]} - {resultTimeMiddlePivot[i]}");
+            }
+            Console.WriteLine($"avarage pivot: avaragetime: { resultTimeAvaragePivot.Sum() / resultTimeAvaragePivot.Count}, min: {resultTimeAvaragePivot.Min()}, max: {resultTimeAvaragePivot.Max()}");
+            Console.WriteLine($"middle pivot: avarage time: { resultTimeMiddlePivot.Sum() / resultTimeMiddlePivot.Count}, min: {resultTimeMiddlePivot.Min()}, max: {resultTimeMiddlePivot.Max()}");
         }
     }
 }
--- Homework4/Vector.cs
+++ Homework4/Vector.cs
@@ -11,6 +11,8 @@ namespace Homework3
     {
         private int[] _array;
         public int Length { get => _array.Length; }
+        public int Max { get => _array.Max(); }
+        public int Min { get => _array.Min(); }
 
         public int this[uint index]
         {
@@ -44,13 +46,11 @@ namespace Homework3
                     for (int i = 0; i < Length - 1; i++)
                     {
                         isSorted = true;
-                        for (int j = 0; j < Length - 1 - i; j++)
+                        for (int j = 0; j < Length - i - 1; j++)
                         {
                             if (_array[j + 1] > _array[j])
                             {
-                                var tmp = _array[j];
-                                _array[j] = _array[j + 1];
-                                _array[j + 1] = tmp;
+                                Swap(ref _array[j], ref _array[j + 1]);
                                 isSorted = false;
                             }
                         }
@@ -66,9 +66,7 @@ namespace Homework3
                         {
                             if (_array[j + 1] < _array[j])
                             {
-                                var tmp = _array[j];
-                                _array[j] = _array[j + 1];
-                                _array[j + 1] = tmp;
+                                Swap(ref _array[j], ref _array[j + 1]);
                                 isSorted = false;
                             }
                         }
@@ -80,10 +78,162 @@ namespace Homework3
                     throw new Exception();
             }
         }
+        public void Counting()
+        {
+            int[] temp = new int[Max - Min + 1];
+            for (int i = 0; i < Length; i++)
+            {
+                temp[_array[i] - Min]++;
+            }
+            int k = 0;
+            for (int i = 0; i < temp.Length; i++)
+            {
+                for (int j = 0; j < temp[i]; j++)
+                {
+                    _array[k] = i + Min;
+                    k++;
+                }
+            }
+        }
+
+        public void QuickSort(Pivot pivotType = Pivot.middleItem, Trend trend = Trend.increase)
+        {
+            uint pivot = 0;
+            switch (pivotType)
+            {
+                case Pivot.firstItem:
+                    pivot = 0;
+                    break;
+                case Pivot.lastItem:
+                    pivot = (uint)Length - 1;
+                    break;
+                case Pivot.middleItem:
+                    pivot = (uint)(Length - 1) / 2;
+                    break;
+                case Pivot.avarage:
+                    pivot = FindAvarage();
+                    break;
+                default:
+                    throw new Exception();
+            }
+            switch (trend)
+            {
+                case Trend.decrease:
+                    QuickSortRecDecrease(0, (uint)Length - 1, pivot);
+                    break;
+                case Trend.increase:
+                    QuickSortRecIncrease(0, (uint)Length - 1, pivot);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void QuickSortRecIncrease(uint firstIndex, uint lastIndex, uint pivot)
+        {
+            if (lastIndex - firstIndex + 1 > 1)
+            {
+                uint i = firstIndex;
+                uint j = lastIndex;
+                while (i < j)
+                {
+                    while (_array[i] <= _array[pivot] && i < pivot)
+                    {
+                        i++;
+                    }
+                    while (_array[j] >= _array[pivot] && j > pivot)
+                    {
+                        j--;
+                    }
+                    if (i < j)
+                    {
+                        Swap(ref _array[i], ref _array[j]);
+                        if (pivot != firstIndex && pivot != lastIndex)
+                        {
+                            j = lastIndex;
+                            i = firstIndex;
+                        }
+                    }
+                }
+                QuickSortRecIncrease(firstIndex, j - 1, (firstIndex + j - 1) / 2);
+                QuickSortRecIncrease(i + 1, lastIndex, (i + 1 + lastIndex) / 2);
+            }
+        }
+        private void QuickSortRecDecrease(uint firstIndex, uint lastIndex, uint pivot)
+        {
+            if (lastIndex - firstIndex + 1 > 1)
+            {
+                uint i = firstIndex;
+                uint j = lastIndex;
+                while (i < j)
+                {
+                    while (_array[i] >= _array[pivot] && i < pivot)
+                    {
+                        i++;
+                    }
+                    while (_array[j] <= _array[pivot] && j > pivot)
+                    {
+                        j--;
+                    }
+                    if (i < j)
+                    {
+                        Swap(ref _array[i], ref _array[j]);
+                        if (pivot != firstIndex && pivot != lastIndex)
+                        {
+                            j = lastIndex;
+                            i = firstIndex;
+                        }
+                    }
+                }
+                QuickSortRecDecrease(firstIndex, j - 1, (firstIndex + j - 1) / 2);
+                QuickSortRecDecrease(i + 1, lastIndex, (i + 1 + lastIndex) / 2);
+            }
+        }
+
+        public bool IsSorted()
+        {
+            for (int i = 0; i < Length - 1; i++)
+            {
+                if (_array[i] > _array[i + 1])
+                    return false;
+            }
+            return true;
+        }
+        private uint FindAvarage()
+        {
+            int sum = _array.Sum();
+            var avg = sum / Length;
+            int k = 1;
+            while (avg + (avg / 3d) * k < Max)
+            {
+                for (uint i = 0; i < Length / 2; i++)
+                {
+                    if (_array[i] < avg + (avg / 3d) * k && _array[i] > avg - (avg / 3d) * k)
+                        return i;
+                    if (_array[Length - i - 1] < avg + (avg / 3d) * k && _array[Length - i - 1] > avg - (avg / 3d) * k)
+                        return (uint)Length - i - 1;
+                }
+                k++;
+            }
+            return 0;
+        }
+        private static void Swap(ref int a, ref int b)
+        {
+            var tmp = a;
+            a = b;
+            b = tmp;
+        }
 
         public void RandomInitialization(int minValue, int maxValue)
         {
             Random random = new Random();
+            for (int i = 0; i < Length; i++)
+            {
+                _array[i] = random.Next(minValue, maxValue);
+            }
+        }
+        public void RandomInitialization(int minValue, int maxValue, int seed)
+        {
+            Random random = new Random(seed);
             for (int i = 0; i < Length; i++)
             {
                 _array[i] = random.Next(minValue, maxValue);
