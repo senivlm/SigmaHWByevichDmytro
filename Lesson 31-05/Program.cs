2263eb111761ae273227ca27a3d34bf5ea093d46
SunnyPiase <petya5671@gmail.com>
2022-05-31 20:13:40 +0300

    added Lesson 31-05

---
+++ Lesson 31-05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_31_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
