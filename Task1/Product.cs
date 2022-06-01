52ae54ecbd3d60119e1b19577e622901e714a882
SunnyPiase <petya5671@gmail.com>
2022-06-01 17:13:34 +0300

    Changed the structure of task 1

---
+++ Task1/Product.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+
+namespace Task1
+{
+    internal class Product
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private double _price;
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Negative price");
+                _price = value;
+            }
+        }
+
+        private double _weight;
+        public double Weight
+        {
+            get { return _weight; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Negative weight");
+                _weight = value;
+            }
+        }
+
+        public Product() : this("name", default, default) { }
+        public Product(string name, double price, double weight)
+        {
+            Name = name;
+            Price = price;
+            Weight = weight;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj == null)
+                throw new ArgumentNullException(nameof(obj));
+            return (obj is  Product other &&
+                    this.Name == other.Name &&
+                    this.Price == other.Price &&
+                    this.Weight == other.Weight);
+
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        public override string? ToString()
+        {
+            return $"Name: {Name}, Price: {Price}, Weight: {Weight}; ";
+        }
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task1
 {
@@ -6,7 +7,46 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Product product1 = new Product();
+            Product product2 = new Product("someProduct", 20.5, 3.4);
+            Product product3 = new Product("Tomato", 15.3, 1);
+            Product product4 = new Product("Milk", 34, 3);
+            try
+            {
+                Product product5 = new Product("NegativPriceProduct", -2, 0);
+                Product product6 = new Product("NegativWeightProduct", 0, -2);
+
+            }
+            catch (ArgumentException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            List<Product> products = new List<Product>();
+
+            products.Add(product1);
+            products.Add(product2);
+            products.Add(product3);
+            products.Add(product4);
+
+
+            Buy buy = new Buy();
+            Buy buy1 = new Buy(products);
+            Buy buy2 = new Buy(products);
+
+            Console.WriteLine(Equals(buy1, buy2));
+            buy2.ProductList.Add(product1);
+            Console.WriteLine(Equals(buy1, buy2));
+
+            buy2.AddProduct(product1);
+            Console.WriteLine(Equals(buy1, buy2));
+
+            Check.PrintCheck(buy);
+            Check.PrintCheck(buy1);
+            Console.WriteLine();
+            Console.WriteLine(buy2);
         }
     }
 }
+
