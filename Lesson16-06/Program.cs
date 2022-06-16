05398fc79186e637835eb2c30d0085d1f1fdf128
SunnyPiase <petya5671@gmail.com>
2022-06-16 18:22:51 +0300

    Task 8_2 Finished

---
+++ Lesson16-06/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson16_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
