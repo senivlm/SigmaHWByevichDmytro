c95f4a36aa889a404357c439523f633032d51520
SunnyPiase <petya5671@gmail.com>
2022-07-09 02:49:02 +0300

    try Fix*

--- Task12_3/Program.cs
+++ Task12_3/Program.cs
@@ -8,37 +8,46 @@ namespace Task12_3
     {
         private static void Main(string[] args)
         {
-            Operations operationsList = new()
+            try
             {
-                new BinaryOperation("+", 1, (a, b) => (a + b)),
-                new BinaryOperation("-", 1, (a, b) => (a - b)),
-                new BinaryOperation("*", 2, (a, b) => (a * b)),
-                new BinaryOperation("/", 2, (a, b) => (a / b)),
-                new BinaryOperation("^", 3, (a, b) => (Math.Pow(a, b))),
-                //унарний мінус
-                new UnaryOperation("=-", 4, a => -a),
-                new UnaryOperation("sin", 4, a => Math.Sin(a)),
-                new UnaryOperation("cos", 4, a => Math.Cos(a)),
-                new UnaryOperation("sqrt", 4, a => Math.Sqrt(a)),
-            };
-
-            List<PolishNotation> polishNotationList = new();
-
-            using (StreamReader sr = new("../../../Files/Task.txt"))
-            {
-                while (sr.EndOfStream == false)
+                Operations operationsList = new()
                 {
-                    polishNotationList.Add(new PolishNotation(sr.ReadLine(), operationsList));
+                    new BinaryOperation("+", 1, (a, b) => (a + b)),
+                    new BinaryOperation("-", 1, (a, b) => (a - b)),
+                    new BinaryOperation("*", 2, (a, b) => (a * b)),
+                    new BinaryOperation("/", 2, (a, b) => (a / b)),
+                    new BinaryOperation("^", 3, (a, b) => (Math.Pow(a, b))),
+                    //унарний мінус
+                    new UnaryOperation("=-", 4, a => -a),
+                    new UnaryOperation("sin", 4, a => Math.Sin(a)),
+                    new UnaryOperation("cos", 4, a => Math.Cos(a)),
+                    new UnaryOperation("sqrt", 4, a => Math.Sqrt(a)),
+                };
+
+                List<PolishNotation> polishNotationList = new();
+
+                using (StreamReader sr = new("../../../Files/Task.txt"))
+                {
+                    while (sr.EndOfStream == false)
+                    {
+                        polishNotationList.Add(new PolishNotation(sr.ReadLine(), operationsList));
+                    }
+                }
+
+                using (StreamWriter sw = new("../../../Files/Report.txt", true))
+                {
+                    foreach (PolishNotation polishNotation in polishNotationList)
+                    {
+                        sw.WriteLine(PolishNotationReportFormatterService.CreateReport(polishNotation));
+                    }
                 }
             }
-
-            using (StreamWriter sw = new("../../../Files/Report.txt", true))
+            catch (Exception ex)
             {
-                foreach (PolishNotation polishNotation in polishNotationList)
-                {
-                    sw.WriteLine(PolishNotationReportFormatterService.CreateReport(polishNotation));
-                }
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
             }
+           
 
         }
     }
